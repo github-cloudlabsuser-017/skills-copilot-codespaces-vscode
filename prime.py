7fb2f84251cc6615c5b4787319ecc3a65decfbba
github-cloudlabsuser-017 <80248330+github-cloudlabsuser-017@users.noreply.github.com>
2024-06-17 06:46:08 +0000

    Copilot second commit

--- prime.py
+++ prime.py
@@ -1,7 +1,7 @@
-def prime(n):    # Add an indented block of code here
+def prime(n):
     if n < 2:
-        return False                        # 2 is the smallest prime number
-        for i in range(2, int(n**0.5) + 1):
+        return False
+    for i in range(2, n):
         if n % i == 0:
             return False
-        return True
+    return
